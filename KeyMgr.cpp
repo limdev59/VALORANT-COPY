d45e59ab0d56d914c51c82b2623d0f9c38f6087e
Lim <bigmanmin1@naver.com>
2024-11-25 13:37:41 +0900

    Update KeyMgr.cpp

--- KeyMgr.cpp
+++ KeyMgr.cpp
@@ -4,7 +4,7 @@
 
 
 int arrVK[(int)KEY::LAST] = {
-    GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_KEY_DOWN,
+    GLUT_KEY_LEFT, GLUT_KEY_RIGHT, GLUT_KEY_UP, GLUT_KEY_DOWN, //πÊ«‚≈∞
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
     'Z', 'X', 'C', 'V', 'B', 'N', 'M',
