0ec586991e64875bfe67956b7d0f584df77e22da
youan04 <ya1041801@gmail.com>
2024-12-15 19:32:22 +0900

    enemt_position

--- Stage_1_Scene.cpp
+++ Stage_1_Scene.cpp
@@ -26,17 +26,26 @@ void Stage_1_Scene::Enter() {
         CObject* player = new Player();
         Model* player_model = new Model(MODEL_TYPE::JETT, GL_TRIANGLES);
         player->setModel(player_model);
-        player->setPosition(vec3(0.0f, 1.0f, 0.0f));
-        player->setScale(vec3(0.3f));
+        player->setPosition(vec3(0.0f, 2.0f, 0.0f));
+        player->setScale(vec3(0.1f));
 
         
+        std::vector<glm::vec3> enemyPositions = {
+            glm::vec3(-0.2f, 2.0f, -0.5f),
+            glm::vec3(1.5f, 2.0f, -0.5f),
+            glm::vec3(-0.2f, 2.0f, 1.8f),
+            glm::vec3(1.5f, 2.0f, 0.1f),
+            glm::vec3(1.2f, 2.0f, 1.8f),
+        };
+
         std::vector<CObject*> enemies; // 적 객체를 관리하는 컨테이너
-        for (int i = 0; i < 5; ++i) {
+
+        for (const auto& position : enemyPositions) {
             Enemy* enemy = new Enemy();
             Model* enemy_model = new Model(MODEL_TYPE::JETT, GL_TRIANGLES); // ENEMY 타입 모델
             enemy->setModel(enemy_model);
-            enemy->setPosition(glm::vec3(-1.0f + i * 0.5f, 1.0f, 0.0f)); // 적의 위치를 다르게 설정
-            enemy->setScale(glm::vec3(0.3f));
+            enemy->setPosition(position); // 적의 위치 설정
+            enemy->setScale(glm::vec3(0.1f));
 
             enemies.push_back(enemy); // 컨테이너에 추가
             addObject(enemy, GROUP_TYPE::ENEMY); // 게임 세계에 등록
