9daac1bdb991dbf579c039031290f603c4e9e472
유안 신 <ya1041801@gmail.com>
2024-12-16 05:38:54 +0900

    jump

--- Player.cpp
+++ Player.cpp
@@ -7,6 +7,9 @@
 #include "CameraMgr.h"
 #include "CCamera.h"
 
+bool isJumping = false; // 점프 상태를 나타냄
+float jumpVelocity = 3.0f; // 초기 점프 속도
+//float maxJumpHeight = 0.75f; // 최대 점프 높이
 
 void Player::Update()
 {
@@ -35,7 +38,10 @@ void Player::Update()
     if (KeyMgr::Instance()->getKeyState(KEY::D) == KEY_TYPE::HOLD) {
         position -= rightVec * static_cast<float>(dt * 1.0);
     }
-
+    if (KeyMgr::Instance()->getKeyState(KEY::SPACE) == KEY_TYPE::HOLD && !isJumping) {
+        isJumping = true;
+        velocity.y = jumpVelocity; // 초기 점프 속도를 적용
+    }
     // 모델 업데이트
     if (model) {
         model->Update(position, rotation, scale);
@@ -49,29 +55,32 @@ void Player::Update()
 
 void Player::Gravity(bool isGra) {
     double dt = DT;
-    if (isGra){
-        velocity.y += gravity * dt;
-        position.y += velocity.y * dt;  // y축에 속도를 더함
 
+    if (isGra) {
+        velocity.y += gravity * dt; // 중력 가속도 적용
+        position.y += velocity.y * dt; // y축 속도를 위치에 반영
+
+        // 바닥과 충돌 처리
         if (position.x < -2.31563 && position.z < -6.84576) {
             if (position.y <= -0.5f) {
                 position.y = -0.5f;
+                velocity.y = 0.0f;
+                isJumping = false; // 점프 종료
             }
-
         }
         else if (position.y <= -0.25f) {
-            
             position.y = -0.25f;
+            velocity.y = 0.0f;
+            isJumping = false; // 점프 종료
         }
-        
 
         if (model) {
             model->Update(position, rotation, scale);
         }
+
         hitboxCenter = position;
         hitboxSize *= scale;
     }
-
     else {
         velocity.y = 0.0f;
     }
