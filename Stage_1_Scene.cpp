54ee4aaed6ccc6bf13718d8a40b46e38ce6ee29f
youan04 <ya1041801@gmail.com>
2024-12-15 12:52:39 +0900

    add_player

--- Stage_1_Scene.cpp
+++ Stage_1_Scene.cpp
@@ -16,14 +16,22 @@ Stage_1_Scene::~Stage_1_Scene() {
 
 void Stage_1_Scene::Enter() {
     if (!loaded) {
-        CObject* obj = new Pearl();
-        Model* model = new Model(MODEL_TYPE::PEARL, GL_TRIANGLES);
-        obj->setModel(model);
-        obj->setScale(vec3(0.1));
+        CObject* map_floor = new Pearl();
+        Model* map_floor_model = new Model(MODEL_TYPE::PEARL, GL_TRIANGLES);
+        map_floor->setModel(map_floor_model);
+        map_floor->setScale(vec3(0.1));
 
-        addObject(obj, GROUP_TYPE::DEFAULT);
+        CObject* player = new Player();
+        Model* player_model = new Model(MODEL_TYPE::JETT, GL_TRIANGLES);
+        player->setModel(player_model);
+        player->setPosition(vec3(0.0f,1.0f,0.0f));
+        player->setScale(vec3(0.3f));
+
+        addObject(map_floor, GROUP_TYPE::DEFAULT);
+        addObject(player, GROUP_TYPE::PLAYER);
         loaded = true;
     }
+
     else {
     }
 }
