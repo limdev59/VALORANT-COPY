6c680f9eb314af6191a4290d6585d85912bcd688
Lim <bigmanmin1@naver.com>
2025-10-25 16:59:55 +0900

    refactor: fragment.glsl

--- Shaders/fragment.glsl
+++ Shaders/fragment.glsl
@@ -7,45 +7,47 @@ in vec3 Normal;
 out vec4 FragColor; 
 
 
-uniform vec3 Ka; // [1단계]
-uniform vec3 Kd; // [2단계]
-uniform vec3 Ks; // [3단계] 주석 해제
-uniform float Ns; // [3단계] 주석 해제
+uniform vec3 Ka;
+uniform vec3 Kd;
+uniform vec3 Ks;
+uniform float Ns;
 
 uniform sampler2D diffuseTexture;
 
 // Lighting
-uniform vec3 lightPos;   // [2단계]
-uniform vec3 lightColor; // [1단계]
-uniform vec3 viewPos;    // [2단계]
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 viewPos;
 
 void main() {
     // 텍스처 색상
     vec3 texColor = texture(diffuseTexture, TexCoords).rgb;
     if (length(texColor) == 0.0) {
-        texColor = vec3(1.0); // 기본 색상(흰색)
+        texColor = vec3(0.5);
     }
 
-    // [2단계] 광원 방향 및 뷰 방향 계산
+    // 광원 방향 및 뷰 방향 계산
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     vec3 viewDir = normalize(viewPos - FragPos);
     
-    // [3단계] 반사 벡터 계산
+    // 반사 벡터
     vec3 reflectDir = reflect(-lightDir, norm);
 
-    // [1단계] Ambient (환경광) 계산
+    // Ambient (환경광) 계산
     vec3 ambient = Ka * lightColor;
 
-    // [2단계] Diffuse (난반사광) 계산
+    // Diffuse (난반사광) 계산
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse =  diff * lightColor ;
 
-    // [3단계] Specular (반사광) 계산
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), Ns);
-    vec3 specular = Ks * spec * lightColor;
 
-    // [3단계] 원본 최종 색상 계산
-    vec3 result = (ambient + diffuse * Kd + specular) * texColor;
-    FragColor = vec4(result, 1.0); // 알파값 1.0
+
+    float shininess = 16.0f;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = Ks  * lightColor * spec;
+
+    // 최종 색상
+    vec3 result = (ambient*1.05 + diffuse * Kd+ specular*0.7) * texColor*1.3 ;
+    FragColor = vec4(result, 1.0);
 }
