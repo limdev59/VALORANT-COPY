4be2c00392ace0caae6c08c4fabdb77715ced382
boowerk <back20044@gmail.com>
2025-11-17 12:43:15 +0900

    feat: add SessionManager.h define
    SessionManager.cpp 구현 필요

---
+++ Server/SessionManager.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "ClientSession.h"
+#include "common.h"
+
+class PacketQueue;
+
+class SessionManager
+{
+public:
+	SessionManager();
+	~SessionManager();
+
+	void Initialize(PacketQueue* worldInQueue);
+	void OnSessionLoggedIn(ClientSession* session, const sockaddr_in& tcpAddr, USHORT clientUdpPort);
+	void OnSessionDisconnected(PlayerID pid);
+	std::vector<ClientSession*> GetAllSessions();
+	PlayerID FindPlayerIDByUdpAddress(const sockaddr_in& addr);
+private:
+	CRITICAL_SECTION m_Lock;
+	std::map<PlayerID, ClientSession*> m_SessionsByPID;
+	PacketQueue* m_pWorldInputQueue;
+	PlayerID m_NextPlayerID;
+};
+
