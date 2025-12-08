71d5b12938e3b9a1e5490f94cc28f418c71ac92d
Lim <bigmanmin1@naver.com>
2025-12-08 14:08:07 +0900

    refactor: Enemy Player

--- Client/Sources/Objects/Enemy.cpp
+++ Client/Sources/Objects/Enemy.cpp
@@ -4,29 +4,120 @@
 #include "CCore.h"
 #include "define.h"
 
+#include "AnimModel.h"
+#include "CameraMgr.h"
+#include "TimeMgr.h"
+#include "CCamera.h"
+
 Enemy::Enemy()
     : CObject() {
-    m_pModel = new IModel<Model>(MODEL_TYPE::JETT, GL_TRIANGLES);
+    
+    // 1. Model / Animation 로드 (Player와 동일하게 구성)
+    string modelName = "jettSimple";
+    string pathName = "jettSimple";
+    m_pModel = new IModel<AnimModel>(modelName, pathName);
+
+    AnimModel* currModel = m_pModel->GetModel(); // 모델에서 본 정보를 가져옴
+
+    // 애니메이션 파일 로드
+    m_pIdleAnim = new Animation("Animations/jettSimple/jettSimple_aim.gltf", currModel);
+    m_pWalkFrontAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkN.gltf", currModel);
+    m_pWalkFrontLeftAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkNW.gltf", currModel);
+    m_pWalkFrontRightAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkNE.gltf", currModel);
+    m_pWalkRightAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkE.gltf", currModel);
+    m_pWalkLeftAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkW.gltf", currModel);
+    m_pWalkBackLeftAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkSW.gltf", currModel);
+    m_pWalkBackRightAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkSE.gltf", currModel);
+    m_pWalkBackAnim = new Animation("Animations/jettSimple/jettSimple_aim_walkS.gltf", currModel);
+
+    // Animator 초기화 (기본 상태: Idle)
+    m_pAnimator = new Animator(m_pIdleAnim);
+
+    // 기본 Transform 설정 (Player와 맞춤)
+    setScale(vec3(0.3f, 0.3f, 0.3f));
+    setRotation(vec3(0, 0.0f, 0.0f));
 }
 
 Enemy::~Enemy() {
     if (m_pModel) delete m_pModel;
+    if (m_pAnimator) delete m_pAnimator;
+    
+    // 애니메이션 리소스 해제
+    if (m_pIdleAnim) delete m_pIdleAnim;
+    if (m_pWalkFrontAnim) delete m_pWalkFrontAnim;
+    if (m_pWalkFrontLeftAnim) delete m_pWalkFrontLeftAnim;
+    if (m_pWalkFrontRightAnim) delete m_pWalkFrontRightAnim;
+    if (m_pWalkLeftAnim) delete m_pWalkLeftAnim;
+    if (m_pWalkRightAnim) delete m_pWalkRightAnim;
+    if (m_pWalkBackAnim) delete m_pWalkBackAnim;
+    if (m_pWalkBackLeftAnim) delete m_pWalkBackLeftAnim;
+    if (m_pWalkBackRightAnim) delete m_pWalkBackRightAnim;
 }
 
 void Enemy::Update() {
+    // 1. 모델 위치/회전/크기 업데이트 (네트워크에서 받은 position, rotation 등이 반영됨)
     if (m_pModel) {
-        m_pModel->GetModel()->Update(position, rotation, scale);
+        m_pModel->GetModel()->UpdateTransform(position, rotation, scale);
     }
 
-    // 히트박스
+    // 2. 애니메이션 업데이트
+    if (m_pAnimator) {
+        m_pAnimator->UpdateAnimation(DT);
+    }
+
+    // 3. 히트박스 업데이트
     hitboxCenter = position;
     hitboxSize = scale;
 }
 
 void Enemy::Render() {
     if (m_pModel) {
-        glUseProgram(CCore::Instance()->shaderProgramID);
-        m_pModel->GetModel()->Render(CCore::Instance()->shaderProgramID);
+        // 애니메이션 셰이더 사용 (Player::Render 참고)
+        glUseProgram(CCore::Instance()->shaderProgramID2);
+        
+        AnimModel* currModel = m_pModel->GetModel();
+
+        // Model 행렬 계산
+        glm::mat4 modelMat = glm::mat4(1.0f);
+        glm::mat4 T = glm::translate(glm::mat4(1.f), position);
+        // Player와 동일 회전 적용 (쿼터니언)
+        glm::mat4 R = glm::mat4_cast(glm::quat(glm::vec3(glm::radians(rotation.x), glm::radians(rotation.y), glm::radians(rotation.z))));
+        glm::mat4 S = glm::scale(glm::mat4(1.f), scale);
+        modelMat = T * R * S;
+
+        modelMat = glm::translate(modelMat, vec3(0.0f, 0.1f, 0.0f));
+
+        // Uniform 전송
+        mat4 projMat = CameraMgr::Instance()->getMainCamera()->getProjectionMatrix();
+        mat4 view = CameraMgr::Instance()->getMainCamera()->getViewMatrix();
+        glm::mat4 PVM = projMat * view * modelMat;
+        glm::mat3 normalMat = glm::mat3(glm::transpose(glm::inverse(modelMat)));
+
+        GLuint loc_modelMat = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "modelMat");
+        GLuint loc_PVM = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "PVM");
+        GLuint loc_normalMat = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "normalMat");
+
+        glUniformMatrix4fv(loc_modelMat, 1, GL_FALSE, glm::value_ptr(modelMat));
+        glUniformMatrix4fv(loc_PVM, 1, GL_FALSE, glm::value_ptr(PVM));
+        glUniformMatrix3fv(loc_normalMat, 1, GL_FALSE, glm::value_ptr(normalMat));
+
+        // Bone(뼈) 행렬 Uniform 전송 (애니메이션 핵심)
+        if (m_pAnimator) {
+            const auto& transforms = m_pAnimator->GetFinalBoneMatrices();
+            GLuint finalBonesMatricesLoc = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "finalBonesMatrices");
+            for (int i = 0; i < transforms.size(); i++) {
+                glUniformMatrix4fv(finalBonesMatricesLoc + i, 1, false, glm::value_ptr(transforms[i]));
+            }
+        }
+
+        // 텍스처 바인딩
+        GLint loc_diffuseSampler = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "diffuseTexture");
+        GLint loc_normalSampler = glGetUniformLocation(CCore::Instance()->shaderProgramID2, "normalMap");
+        glUniform1i(loc_diffuseSampler, 0);
+        glUniform1i(loc_normalSampler, 1);
+
+        // 그리기
+        currModel->Render();
     }
 }
 
@@ -40,5 +131,3 @@ void Enemy::TakeDamage(int damage) {
 void Enemy::OnDeath() {
     std::cout << "Enemy died!" << std::endl;
 }
-
-
